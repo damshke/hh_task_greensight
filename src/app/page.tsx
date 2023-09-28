86fe42f908dc95f36d5504edcf179a07ccd8e5af
damshke <degorych25@mail.ru>
2023-09-28 22:49:47 +0300

    start work on the test task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,51 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+
+import { useState, useEffect } from 'react'
+import Header from './components/Header';
+
+type resultProps = {
+  id: string;
+  name: string;
+};
+
 
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+
+  const [data, setData] = useState<resultProps[]>([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('https://api.hh.ru/vacancies?page=1')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.items)
+        setLoading(false)
+      })
+  }, [])
+
+
+  if (!data) {
+    return (
+      <div>
+        <Header />
+        <p>No vacancies found.</p>
+      </div>
+    );
+  }
+
+  else {
+    return (
+      <div>
+        <Header />
+
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>
+              <p>{item.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
